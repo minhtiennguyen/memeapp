405e4a1d2dff2f4cdd6e52e111fa4eae237eaed4
Minh Tien Nguyen <7450605@student.swin.edu.au>
2016-08-04 13:43:29 +0700

    Initialise setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+var ProfileBox = React.createClass({
+	render: function() {
+		return(
+			<p> Hello</p>
+		);
+	}
+});
+
+ReactDOM.render(
+	<ProfileBox />, 
+	document.getElementById('container')
+);
